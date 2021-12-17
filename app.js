69ed95b9d1bdb3319ac55f98dece9e18b3e2e9a9
Smlcrp <smlcrp@gmail.com>
2021-12-17 11:06:39 -0500

    Commit 1 - Verbal Questions

---
+++ app.js
@@ -0,0 +1,7 @@
+/*
+* 1. Parameters are the placeholder for the function to set value to with the argument.
+* 
+* 2. Return is the result of a function and Console.log is just a way to print to the console
+*
+* 3. The ability of a function to return values implies that you can use the returned values of functions to solve other functions to solve complex problems 
+*/
